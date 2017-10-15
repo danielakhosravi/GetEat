1d50d9cb106500eb5e867bf464bf2338ec512d4c
danielakhosravi <danielayurukova@yahoo.com>
2017-10-15 20:51:40 +0300

    Layout & Home Page

---
+++ GetEat/GetEat.WebUI/Scripts/geteat/home.js
@@ -0,0 +1,46 @@
+﻿
+$(document).ready(function () {
+
+    // autocompelete
+    var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+    $("#tags").autocomplete({
+        source: availableTags
+    });
+
+    // date picker intialization 
+
+    //$('#datetimepicker1').datetimepicker();
+    ////
+    //$('#timepicker2').timepicker({
+    //    minuteStep: 1,
+    //    template: 'modal',
+    //    appendWidgetTo: 'body',
+    //    showSeconds: true,
+    //    showMeridian: false,
+    //    defaultTime: false
+    //});
+
+});
